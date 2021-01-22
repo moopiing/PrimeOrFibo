2027c986f99a9d46a8a5e289f6ebee66ea8df6c8
visurt <visurt.antv@gmail.com>
2021-01-22 17:18:12 +0700

    Initialized Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'q1';
+  result: any = ""
+  num: any = "";
+  mode: string = "isPrime";
+  options = [
+    { name: "isPrime" },
+    { name: "isFibonacci" }
+  ]
+
+  onChange(e) {
+    this.num = Math.round(e.target.value)
+    if (this.num < 0) this.num = 1
+    this.checkResult(this.num)
+  }
+
+  onSelect(e) {
+    this.checkResult(this.num)
+  }
+
+  checkResult(num) {
+    if (this.mode == 'isPrime')
+      this.result = this.isPrime(num)
+
+    if (this.mode == 'isFibonacci')
+      this.result = this.isFibonacci(num)
+  }
+
+  isPrime(num) {
+    for(var i = 2; i < num; i++)
+      if(num % i === 0) return false;
+    return num > 1;
+  }
+
+  isFibonacci(n) {
+    let a = (5 * Math.pow(n, 2) + 4)
+    let b = (5 * Math.pow(n, 2) - 4)
+  
+    let result = Math.sqrt(a) % 1 == 0,
+    res = Math.sqrt(b) % 1 == 0;
+  
+    if (result || res == true)
+      return true;
+    return false;
+  }
 }
